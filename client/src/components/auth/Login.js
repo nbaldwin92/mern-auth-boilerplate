3828f5d6c24d620321c576b7b0a8c6d7bac89ca6
Noah Baldwin <nbaldwin3689@flpoly.org>
2019-04-27 08:21:03 -0400

    Convert login and register classes to hooks

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,112 +1,89 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/authActions';
 
-class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      password: '',
-      errors: {},
-    };
-  }
+const Login = props => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  componentDidMount() {
+  // eslint-disable-next-line no-shadow
+  const { auth, history, loginUser, errors } = props;
+
+  useEffect(() => {
     // If logged in and user navigates to Login page, should redirect them to dashboard
-    // eslint-disable-next-line react/prop-types
-    const { auth, history } = this.props;
     if (auth.isAuthenticated) {
       history.push('/dashboard');
     }
-  }
+  });
 
-  // TODO Don't use this method - Handle errors and display to client
-  componentWillReceiveProps(nextProps) {
-    // eslint-disable-next-line react/prop-types
-    const { history } = this.props;
-    if (nextProps.auth.isAuthenticated) {
-      history.push('/dashboard');
-    }
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    const { email, password } = this.state;
 
     const userData = {
       email,
       password,
     };
 
-    // eslint-disable-next-line react/destructuring-assignment
-    this.props.loginUser(userData);
+    loginUser(userData);
   };
 
-  render() {
-    const { email, password } = this.state;
-    const { errors } = this.props;
-    return (
-      <div className="container">
-        <div style={{ marginTop: '4rem' }} className="row">
-          <div className="col-lg-12">
-            <Link to="/">Back to home</Link>
-            <div style={{ paddingLeft: '11.250px' }}>
-              <h4>
-                <b>Login</b> below
-              </h4>
-              <p>
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-
-            <form>
-              <p>Sign in</p>
-              <p>{errors.email}</p>
-              <p>{errors.emailnotfound}</p>
-              <p>{errors.password}</p>
-              <p>{errors.passwordincorrect}</p>
-              <div>
-                <input
-                  className="form-control"
-                  placeholder="Email"
-                  type="email"
-                  id="email"
-                  onChange={this.onChange}
-                  value={email}
-                />
-                <input
-                  className="form-control"
-                  placeholder="Password"
-                  type="password"
-                  id="password"
-                  onChange={this.onChange}
-                  value={password}
-                />
-              </div>
-              <div className="text-center">
-                <button type="submit" onClick={this.onSubmit}>
-                  Login
-                </button>
-              </div>
-            </form>
+  return (
+    <div className="container">
+      <div style={{ marginTop: '4rem' }} className="row">
+        <div className="col-lg-12">
+          <Link to="/">Back to home</Link>
+          <div style={{ paddingLeft: '11.250px' }}>
+            <h4>
+              <b>Login</b> below
+            </h4>
+            <p>
+              Don't have an account? <Link to="/register">Register</Link>
+            </p>
           </div>
+
+          <form>
+            <p>Sign in</p>
+            <p>{errors.email}</p>
+            <p>{errors.emailnotfound}</p>
+            <p>{errors.password}</p>
+            <p>{errors.passwordincorrect}</p>
+            <div>
+              <input
+                className="form-control"
+                placeholder="Email"
+                type="email"
+                id="email"
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+              />
+              <input
+                className="form-control"
+                placeholder="Password"
+                type="password"
+                id="password"
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
+            <div className="text-center">
+              <button type="submit" onClick={onSubmit}>
+                Login
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Login.propTypes = {
   loginUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
 };
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,49 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
 
-class Register extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      email: '',
-      password: '',
-      password2: '',
-      // eslint-disable-next-line react/no-unused-state
-      errors: {},
-    };
-  }
+const Register = props => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
 
-  componentDidMount() {
+  // eslint-disable-next-line no-shadow
+  const { auth, history, registerUser, errors } = props;
+
+  useEffect(() => {
     // If logged in and user navigates to Register page, should redirect them to dashboard
-    // eslint-disable-next-line react/prop-types
-    const { auth, history } = this.props;
     if (auth.isAuthenticated) {
       history.push('/dashboard');
     }
-  }
+  });
 
-  // TODO Don't use this method - handle errors better and display to client
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({
-        errors: nextProps.errors,
-      });
-    }
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
 
-    const { name, email, password, password2 } = this.state;
-    const { history } = this.props;
+    // eslint-disable-next-line no-shadow
 
     const newUser = {
       name,
@@ -52,81 +32,75 @@ class Register extends Component {
       password2,
     };
 
-    // eslint-disable-next-line react/destructuring-assignment
-    this.props.registerUser(newUser, history);
+    registerUser(newUser, history);
   };
 
-  render() {
-    const { name, email, password, password2 } = this.state;
-    const { errors } = this.props;
-
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-6">
-            <Link to="/">Back to home</Link>
-            <div style={{ paddingLeft: '11.250px' }}>
-              <h4>
-                <b>Register</b> below
-              </h4>
-              <p>
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
-            </div>
-
-            <form>
-              <p>Sign up</p>
-              <div>
-                <p>{errors.email}</p>
-                <p>{errors.name}</p>
-                <p>{errors.password}</p>
-                <p>{errors.password2}</p>
-                <input
-                  className="form-control"
-                  placeholder="Name"
-                  type="text"
-                  id="name"
-                  onChange={this.onChange}
-                  value={name}
-                />
-                <input
-                  className="form-control"
-                  placeholder="Email"
-                  type="email"
-                  id="email"
-                  onChange={this.onChange}
-                  value={email}
-                />
-
-                <input
-                  className="form-control"
-                  placeholder="Password"
-                  type="password"
-                  id="password"
-                  onChange={this.onChange}
-                  value={password}
-                />
-                <input
-                  className="form-control"
-                  placeholder="Confirm Password"
-                  type="password"
-                  id="password2"
-                  onChange={this.onChange}
-                  value={password2}
-                />
-              </div>
-              <div className="text-center">
-                <button type="submit" onClick={this.onSubmit}>
-                  Register
-                </button>
-              </div>
-            </form>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6">
+          <Link to="/">Back to home</Link>
+          <div style={{ paddingLeft: '11.250px' }}>
+            <h4>
+              <b>Register</b> below
+            </h4>
+            <p>
+              Already have an account? <Link to="/login">Log in</Link>
+            </p>
           </div>
+
+          <form>
+            <p>Sign up</p>
+            <div>
+              <p>{errors.email}</p>
+              <p>{errors.name}</p>
+              <p>{errors.password}</p>
+              <p>{errors.password2}</p>
+              <input
+                className="form-control"
+                placeholder="Name"
+                type="text"
+                id="name"
+                onChange={e => setName(e.target.value)}
+                value={name}
+              />
+              <input
+                className="form-control"
+                placeholder="Email"
+                type="email"
+                id="email"
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+              />
+
+              <input
+                className="form-control"
+                placeholder="Password"
+                type="password"
+                id="password"
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+              />
+              <input
+                className="form-control"
+                placeholder="Confirm Password"
+                type="password"
+                id="password2"
+                onChange={e => setPassword2(e.target.value)}
+                value={password2}
+              />
+            </div>
+            <div className="text-center">
+              <button type="submit" onClick={onSubmit}>
+                Register
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
